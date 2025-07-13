7688ad378e3dcc783df2a9a5cdfa21eb4ceae075
naem-ali <naeem.ali@gmx.de>
2025-07-13 18:19:12 +0200

    adding_Security_Groups

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -10,9 +10,21 @@ module "vpc" {
   project              = "demo"
   public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnet_cidrs = ["10.0.3.0/24", "10.0.4.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true  # Use single NAT Gateway for dev environment to save costs
+}
+
+module "security_groups" {
+  source = "../../modules/security_groups"
+
+  vpc_id           = module.vpc.vpc_id
+  env              = "dev"
+  project          = "demo"
+  allowed_ssh_cidrs = ["10.0.0.0/8"]  # Restrict SSH access to internal network
 }
 
 module "ec2" {
   source = "../../modules/ec2"
   env    = "dev"
+  vpc_security_group_ids = [module.security_groups.web_security_group_id]
 }
--- infra/modules/vpc/variables.tf
+++ infra/modules/vpc/variables.tf
@@ -22,3 +22,15 @@ variable "private_subnet_cidrs" {
   description = "CIDR blocks for private subnets"
   type        = list(string)
 }
+
+variable "enable_nat_gateway" {
+  description = "Enable NAT Gateway for private subnets"
+  type        = bool
+  default     = true
+}
+
+variable "single_nat_gateway" {
+  description = "Use a single NAT Gateway for all private subnets"
+  type        = bool
+  default     = false
+}
